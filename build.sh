70719708392d54ca262cc0d18b45728f4a06b411
Ella Moskun <ella.moskun@utah.edu>
2022-04-18 17:55:25 -0600

    also build tarball

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-rm -f Shiny.AppImage
+rm -f Shiny-x86_64.AppImage
+rm -f Shiny-x86_64.tar.gz
 ARCH=x86_64 ./appimagetool-x86_64.AppImage -v -n ./Shiny.AppDir
+cd ./Shiny.AppDir
+tar --exclude='DirIcon' --exclude='AppRun' --exclude='Shiny.desktop' -czvf Shiny-x86_64.tar.gz *
+cd ..
+mv ./Shiny.AppDir/Shiny-x86_64.tar.gz ./Shiny-x86_64.tar.gz
