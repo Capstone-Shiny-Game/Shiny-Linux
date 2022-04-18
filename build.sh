5e73e802dd4a627829b97a73c03ffff2e1d90b49
Ella Moskun <ella.moskun@utah.edu>
2022-04-18 17:29:20 -0600

    fix script

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
-rm -f Shiny.squashfs
 rm -f Shiny.AppImage
-mksquashfs Shiny.AppDir Shiny.squashfs -root-owned -noappend
-cat runtime >> Shiny.AppImage
-cat Shiny.squashfs >> Shiny.AppImage
-chmod a+x Shiny.AppImage
+ARCH=x86_64 ./appimagetool-x86_64.AppImage -v -n ./Shiny.AppDir
---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install -y fuse squashfs-tools wget
+wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod a+x appimagetool-x86_64.AppImage
