bba8a90d1a2c96c8889e4ea8a91a82a17746be95
Ella Moskun <ella.moskun@utah.edu>
2022-04-18 20:27:57 -0600

    mark actual executable as such

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm -f Shiny-x86_64.AppImage
 rm -f Shiny-x86_64.tar.gz
+chmod a+x ./Shiny.AppDir/Shiny.x86_64
 ARCH=x86_64 ./appimagetool-x86_64.AppImage -v -n ./Shiny.AppDir
 cd ./Shiny.AppDir
 tar --exclude='DirIcon' --exclude='AppRun' --exclude='Shiny.desktop' -czvf Shiny-x86_64.tar.gz *
